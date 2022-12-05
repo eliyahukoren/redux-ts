d2a03951c128cc7ecdf443ffef6f66228da58c40
eliyahukoren <elik.koren@gmail.com>
2022-12-05 13:41:01 +0200

    make show errors, list of packages a bit nice

--- src/components/repositories-list/RepositoriesList.tsx
+++ src/components/repositories-list/RepositoriesList.tsx
@@ -1,26 +1,30 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import { useActions } from "../../hooks/useActions";
 import { useTypedSelector } from "../../hooks/useTypedSelector";
 
-const RepositoriesList:React.FC = () => {
+const RepositoriesList: React.FC = () => {
 	const refTerm = useRef<HTMLInputElement>(null);
-	const { searchRepositories }  = useActions();
-	const {data, loading, error} = useTypedSelector((state) => state.repositories);
-
-	console.log(loading, error, data);
+	const [displayText, setDisplayText ] = useState('');
+	const { searchRepositories } = useActions();
+	const { data, loading, error } = useTypedSelector(
+		(state) => state.repositories
+	);
+	const [showNotFound, setShowNotFound] = useState(false);
 
 	useEffect(() => {
-		if( refTerm.current ){
+		if (refTerm.current) {
 			refTerm.current.focus();
 		}
 	}, []);
 
+	useEffect(()=>{
+		setShowNotFound(!loading && !error && data.length === 0 && displayText !== '')
+	}, [data, loading, error, displayText])
+
 	const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-		if( refTerm.current?.value ){
+		if (refTerm.current?.value) {
 			const text = refTerm.current?.value;
-			refTerm.current.value = '';
-			console.log(`text is: ${text}`)
 			searchRepositories(text);
 		}
 	};
@@ -35,29 +39,55 @@ const RepositoriesList:React.FC = () => {
 
 							<p className="card-text">
 								Simple Redux typescript application for search npm
-								packages
+								packages <strong>{displayText}</strong>
 							</p>
+
+							{error ? (
+								<div className="alert alert-danger mb-3" role="alert">
+									{error}
+								</div>
+							) : null}
+
+							{loading ? (
+								<div
+									className="spinner-border text-info mb-3"
+									role="status"
+								>
+									<span className="visually-hidden">Loading...</span>
+								</div>
+							) : null}
 
 							<input
 								ref={refTerm}
+								onChange={(e) => {setDisplayText(e.target.value); setShowNotFound(false);}}
 								type="text"
 								className="form-control mb-3"
 								placeholder="Type package name"
 							/>
 
-							<ul className="list-group text-start mb-3">
-								<li className="list-group-item">package 1</li>
-								<li className="list-group-item">package 1</li>
-								<li className="list-group-item">package 1</li>
-							</ul>
+							<button className="btn btn-primary mb-3">Search</button>
 
-							<button className="btn btn-primary">Search</button>
+							{!loading && !error && data.length !== 0 ? (
+								<ul className="list-group text-start mb-3">
+									{data.map((item, index) => (
+										<li key={index} className="list-group-item">
+											{item}
+										</li>
+									))}
+								</ul>
+							) : null}
+
+							{ showNotFound ? (
+								<div className="alert alert-primary" role="alert">
+									Not found packages <strong>{displayText}</strong>
+								</div>
+							) : null}
 						</div>
 					</div>
 				</form>
 			</div>
 		</div>
 	);
-}
+};
 
 export default RepositoriesList;
