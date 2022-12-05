d959280e23c9196993816415f5cc1669e607978e
eliyahukoren <elik.koren@gmail.com>
2022-12-05 09:39:59 +0200

    create action creator

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { Dispatch } from "redux";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+const url = "https://registry.npmjs.org/-/v1/search";
+
+const searchRepositories = (text: string) => {
+	return async (dispatch: Dispatch<Action>) => {
+		dispatch({
+			type: ActionType.SEARCH_REPOSITORIES,
+		});
+
+		try {
+			const { data } = await axios.get(url, {
+				params: { text },
+			});
+
+			const names = data.object.map((result: any) => {
+				return result.package.name
+			});
+
+			dispatch({
+				type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+				payload: names
+			});
+
+		} catch (err) {
+			if (err instanceof Error) {
+				dispatch({
+					type: ActionType.SEARCH_REPOSITORIES_ERROR,
+					payload: err.message,
+				});
+			}
+		}
+	};
+};
